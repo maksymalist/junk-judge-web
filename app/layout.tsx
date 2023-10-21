122cae25a3edd1b2ea7a2e09cdbe7d3b8805163c
maksymalist <maksymvovkk@gmail.com>
2023-10-21 09:57:38 -0400

    added a banner for links

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <meta property="og:image" content="/assets/banner"></meta>
+      <meta property="og:image" content="/assets/banner.png"></meta>
       <body className={inter.className}>{children}</body>
     </html>
   );
